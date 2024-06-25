0bfca66cf06b7bb93fa5b256b319b32d56e8f2c5
Jaime Linares <jaime.linares@prediktia.ai>
2024-06-24 21:47:46 -0500

    add google maps map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 "use client";
-
-// importing necessary functions
 import { useSession, signIn, signOut } from "next-auth/react";
-
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import { useEffect, useState } from "react";
+const mapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
+console.log({ mapsApiKey });
 export default function Home() {
   const { data: session } = useSession();
+
+  const position = { lat: 53.54992, lng: 10.00678 };
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   // checking if sessions exists
   if (session) {
@@ -20,11 +29,19 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main style={{ height: "100vh" }}>
+      {/* 
       <h1>HomePage</h1>
       <button onClick={() => handleButton()}>
         {session ? "signOut" : "signIn"}
-      </button>
+      </button> */}
+      {isClient && (
+        <APIProvider apiKey={mapsApiKey}>
+          <Map defaultCenter={position} defaultZoom={10}>
+            <Marker position={position} />
+          </Map>
+        </APIProvider>
+      )}
     </main>
   );
 }
