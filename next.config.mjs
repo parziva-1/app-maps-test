155203b9e2ee24b71e32595e0e16bb3a7e09d4b3
Jaime Linares <jaime.linares@prediktia.ai>
2024-06-27 14:46:02 -0500

    add styles

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["mongoose"],
   },
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
 };
 
 export default nextConfig;
--- src/components/adminPanel/avatarContainer/avatarContainer.tsx
+++ src/components/adminPanel/avatarContainer/avatarContainer.tsx
@@ -1,5 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "./button";
+import Image from "next/image";
+import styles from "./avatarContainer.module.css";
 
 const AvatarContainer = () => {
   const { data: session } = useSession();
@@ -18,11 +20,18 @@ const AvatarContainer = () => {
   };
 
   return (
-    <section>
+    <section className={styles.avatarContainer}>
       {session ? (
         <>
-          <p>{session?.user?.name}</p>
-          <p>{session?.user?.email}</p>
+          <p className={styles.userName}>{session?.user?.name}</p>
+          <p className={styles.userEmail}>{session?.user?.email}</p>
+          <Image
+            src={session.user?.image ?? ""}
+            alt="avatar-image"
+            className={styles.avatarImage}
+            width={80}
+            height={80}
+          />
         </>
       ) : null}
       {session ? (
