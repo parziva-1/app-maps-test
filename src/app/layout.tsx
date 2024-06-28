62355f7972eca9a606422ef4be78aa54d380ac0d
Jaime Linares <jaime.linares@prediktia.ai>
2024-06-27 20:58:12 -0500

    add react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionWrapper } from "@/components/sessionWrapper";
+import { SessionProvider } from "@/providers/sessionProvider";
+import { ReactQueryProvider } from "@/providers/reactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <SessionWrapper>
+    <SessionProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </body>
       </html>
-    </SessionWrapper>
+    </SessionProvider>
   );
 }
